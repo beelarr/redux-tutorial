00bc7b42dff5229e2898f1937d99187d7dd845bc
beelarr <bryonl@me.com>
2018-01-15 16:16:54 -0600

    238

--- redux-basics.js
+++ redux-basics.js
@@ -7,7 +7,21 @@ const initialState = {
 
 // Reducer
 
-const rootReducer = (state = initialState, action) =>  state;
+const rootReducer = (state = initialState, action) =>  {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+    return state
+};
 
 // Store
 const store = createStore(rootReducer);
@@ -17,5 +31,11 @@ console.log(store.getState());
 
 
 // Dispatch Action
+
+
+store.dispatch({type: 'INC_COUNTER'}); // type is at my discretion but typically all caps
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
+
 
 // Subscription
