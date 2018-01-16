e0723035af90a725f28313dc2ef0316b8b70d499
beelarr <bryonl@me.com>
2018-01-16 14:27:37 -0600

    244

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,28 +3,30 @@ const initialState = {
 };
 
 const reducer = ( state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return{
-            ...state,
-            counter: state.counter + 1
-        }
-    } else if (action.type === 'DECREMENT') {
-        return {
-            ...state,
-            counter: state.counter -1
-        }
-    } else if (action.type === 'ADD') {
-        return {
-            ...state,
-            counter: state.counter + action.payload.value
-        }
-    } else if (action.type === 'SUBTRACT') {
-        return {
-            ...state,
-            counter: state.counter - action.payload.value
-        }
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter -1
+            };
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.payload.value
+            };
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.payload.value
+            }
     }
-    return state;
+    return state;  // if no cases are present return current state so app doesn't break
 };
 
 export default reducer
